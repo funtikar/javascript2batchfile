5d370883a511b8a8d3a48a0a4ac28c6d5460201f
funtikar <funtikar@gmail.com>
2015-03-20 11:25:40 +0800

    Adding token list

--- meleiscript/mls.py
+++ meleiscript/mls.py
@@ -1,5 +1,6 @@
 from sys import *
 
+tokens = []
 def buka(argi):
 	buff = open(argi,"r").read()
 	return buff
@@ -12,26 +13,30 @@ def lexer(argi):
 	for aksara in data:
 		buffa = buffa + aksara
 		if (buffa == "print"):
-			print "[debugging] it was a print"
+			#print "[debugging] it was a print"
 			buffa = ""
 		if (buffa == "if"):
-			if (if_state == 0):
-				if_state = 1
-		if (if_state == 1 and buffa == "("):
-			
+			tokens.append("IF")
+			buffa == ""
+		if (buffa == "("):
+			tokens.append("L_CURVE")
+			buffa == ""
 		if(aksara == "\""):
-			print "[debugging] it was a string!"
+			#print "[debugging] it was a string!"
 			if (str_state == 0):
 				str_state = 1
 			elif (str_state == 1):
 				str_state = 0
 				str_buff = buffa
+				tokens.append(buffa[1:-1])
 				buffa = ""
-				print str_buff
 		if (aksara == " " and str_state == 0):
 			buffa = ""
+		if (aksara == "\n"):
+			buffa == ""
 def utama():
 	lexer(buka("contoh.my"))
+	print tokens
 	
 #supposed to be this one down here
 #lexer(buka(argv[1]))
